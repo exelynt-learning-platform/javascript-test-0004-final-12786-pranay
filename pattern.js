71936a4b50813700231ea1cffcbfd5e88ddb4171
pranaybhoware <pranaybhoware2701@gmail.com>
2026-03-14 14:19:45 +0530

    solution done

---
+++ pattern.js
@@ -0,0 +1,25 @@
+function printPattern(n) {
+
+    let size = 2 * n - 1;
+
+    for (let i = 0; i < size; i++) {
+
+        let row = "";
+
+        for (let j = 0; j < size; j++) {
+
+            let top = i;
+            let left = j;
+            let bottom = size - 1 - i;
+            let right = size - 1 - j;
+
+            let min = Math.min(top, left, bottom, right);
+
+            row += (n - min) + " ";
+        }
+
+        console.log(row.trim());
+    }
+}
+
+printPattern(4);
